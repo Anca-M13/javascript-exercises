e7d848a3302eca05b9420d4740e07ef6deb71fc2
Mirela E <ancamirelam@gmail.com>
2023-04-24 14:49:23 +0300

    Revested string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let newString = ''; 
+  for(i = 1; i <= string.length; i++) {
+    let lastCh = string[string.length - i];
+    newString += lastCh;
+  }
+  return newString;
 };
 
 // Do not edit below this line
