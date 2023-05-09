16e5aacd710557ec5b10b97b5cfcc8ba24eef134
Mirela E <ancamirelam@gmail.com>
2023-05-09 18:31:42 +0300

    done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
-
-};
+const sumAll = function(a, b) {
+  if (typeof a === 'number' & typeof b === 'number') {
+    if (a >= 0 & b >=0) {
+      let total = 0;
+      if (a < b) {
+        for (i = a; i <= b; i++) {
+            total += i;
+        }
+        return total;
+      } else {
+        for (i = b; i <= a; i++) {
+          total += i;
+        }
+        return total;  
+      }
+    } else {
+        return 'ERROR'
+    }
+  } else {
+    return 'ERROR'
+  }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
