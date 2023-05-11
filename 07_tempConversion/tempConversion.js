ce06c065eda824327e8f6e41ff781a1fd993d0bc
Mirela E <ancamirelam@gmail.com>
2023-05-11 11:50:04 +0300

    done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let newTemp = (tempF - 32) * (5 / 9);
+  newTemp = Math.round (newTemp * 10) / 10;
+  return newTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  return Math.round((tempC * (9 / 5) +32) * 10) / 10;
 };
 
 // Do not edit below this line
